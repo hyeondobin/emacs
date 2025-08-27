b88348a1e11f8991d15066359523826e7e1bb5d2
Hyeondobin <dobinhyeon@gmail.com>
2025-08-27 13:50:33 +0900

    add function to open init file with keybinding

--- config/keybinds.el
+++ config/keybinds.el
@@ -1,4 +1,9 @@
-(global-set-key (kbd "C-c i") '(find-file ~/.emacs.d/init.el))
+(defun dobin/open-init-file ()
+  "Open init.el"
+  (interactive)
+  (find-file user-init-file))
+
+(global-set-key (kbd "C-c i") 'dobin/open-init-file)
 (global-set-key (kbd "C-c s") 'eshell)
 (global-set-key (kbd "C-c C-s") 'eshell)
 

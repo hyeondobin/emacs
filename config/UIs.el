ac211bd597e519f3290b510d4f29c9d6e83e238c
hyeondobin <dobinhyeon@gmail.com>
2025-09-12 21:05:17 +0900

    disable tab-bar-mode

--- config/UIs.el
+++ config/UIs.el
@@ -1,7 +1,7 @@
 (menu-bar-mode -1)
 (tool-bar-mode -1)
 (scroll-bar-mode -1)
-(tab-bar-mode)
+(tab-bar-mode -1)
 
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))

bbf5190df57367cc3155acda003ba8562528cb31
hyeondobin <dobinhyeon@gmail.com>
2025-09-14 12:06:16 +0900

    한글 폰트를 D2Coding NF, 영문 폰트를 JetBrainsMono NF로 설정

--- config/fonts.el
+++ config/fonts.el
@@ -1,23 +1,62 @@
+;; config about fonts
+(defvar dobin/default-font (font-spec 
+			    ;; :family "JetBrainsMono Nerd Font"
+			    :family "NanumGothicCoding"
+			    :height 1
+			    ))
+
+(defvar dobin/kor-font (font-spec
+			:family "D2CodingLigature Nerd Font"
+			;; :family "D2Coding"
+			:registry "unicode-bmp"
+			;; :width 'wide
+			:size 20
+			))
+
 (set-face-attribute 'default nil 
-		    :height 160  ;; 기본 베이스가 되는 크기 설정
-		    :family "JetBrainsMono Nerd Font")
+		    :height 150  ;; 베이스가 기본 되는 크기 설정 
+		    :family "JetBrainsMono Nerd Font"
+		    ;; :family "D2Coding"
+		    )
+(set-fontset-font "fontset-default" '(#xac00 . #xd7a3) dobin/kor-font)
+		  ;; dobin/kor-font)
 
-;;(add-to-list 'default-frame-alist '(font . "D2CodingLigature Nerd Font"))
-
-;;(set-fontset-font nil 'hangul (font-spec :family "D2Coding" :pixelsize 12))
-(setq face-font-rescale-alist '((".*D2Coding.*" . 1.24)))
-(setq-default line-spacing 3)
 (set-fontset-font "fontset-default" '(#x1100 . #xffdc) ;; 유니코드 한글 영역 1001234560798
-		  (font-spec :family "D2CodingLigature Nerd Font" :pixelsize 15 :registry "iso10646-1"))
-		  ;; '("D2CodingLigature Nerd Font" . "iso10646-1"))
-(set-fontset-font "fontset-default" '(#xe0bc . #xf66e) ;; 유니코드 사용자 영역
-		  '("D2Coding" . "iso10646-1"))
+		  ;; (font-spec :family "NanumGothicCoding")
+		  dobin/kor-font
+		  )
+
+(add-to-list 'face-font-rescale-alist '(".*D2Coding.*" . 1.23))
+(add-to-list 'face-font-rescale-alist '(".*NanumGothic.*" . 1.25))
+
+(setq-default line-spacing 4)
 
 (provide 'fonts)
-
 ;; | 1564 |
 ;; | 한글 | 이것은 한글 입력 예시Is Korean Tall입니다er than English. |
 ;; | abcd | this is sample text input yes|
+
+;; 01234567890123456789012345678901234567890
+;; The quick brown fox jumps over a lazy dog.
+;; 다람쥐 헌 쳇바퀴에 타고파
+
+;; All questions asked by five watch experts amazed the judge.
+;; 키스의 고유 조건은 입술끼리 만나야 하고 특별한 요령은 필요치 않다.
+
+;; SIMILAR = "oO08ㅇ iIlL1 g9qCGQ 8%& <([{}])> .,;: -_="
+
+
+;;(add-to-list 'default-frame-alist '(font . "D2CodingLigature Nerd Font"))
+
+;; (set-fontset-font nil 'hangul (font-spec :family "D2Coding" :pixelsize 12))
+;; (set-fontset-font "fontset-default" '(#x1100 . #xffdc)
+		  ;; (font-spec :family "D2Coding") )
+		  ;; '("D2CodingLigature Nerd Font" . "iso10646-1"))
+		  ;; '("JetBrainsMono Nerd Font" . "iso10646-1"))
+
+;; (setq face-font-rescale-alist '((".*D2Coding.*" . 1.24)))
+
+
 
 
 

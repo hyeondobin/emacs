c2396965b7d43638b4ac4c725e198413ea3b5be9
hyeondobin <dobinhyeon@gmail.com>
2025-09-14 12:07:24 +0900

    Add "Rainbow Delimiter"
    set scroll margin to 10
    add toggle frame maximized to after init hook

--- config/UIs.el
+++ config/UIs.el
@@ -6,4 +6,13 @@
 (add-to-list 'default-frame-alist '(fullscreen . maximized))
 (add-to-list 'initial-frame-alist '(fullscreen . maximized))
 
+(add-hook 'after-init-hook 'toggle-frame-maximized)
+
+(setq scroll-margin 10)
+
+;; rainbow delimiter : 괄호 무지개 색으로 묶어서 색칠해줌
+(use-package rainbow-delimiters
+  :hook
+  (prog-mode . rainbow-delimiters-mode))
+
  (provide 'UIs)

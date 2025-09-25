07b730606532fe750c990cda78d4c7439115845f
hyeondobin <dobinhyeon@gmail.com>
2025-09-25 15:52:24 +0900

    update config

--- init.el
+++ init.el
@@ -11,6 +11,8 @@
 (scroll-bar-mode -1)
 (setq display-line-numbers-type 'relative)
 (add-hook 'prog-mode-hook #'display-line-numbers-mode)
+(add-hook 'org-mode-hook #'display-line-numbers-mode)
+(setq ring-bell-function #'ignore)
 
 (setq scroll-margin 10)
 
@@ -66,6 +68,129 @@
   (catppuccin-reload)
   (load-theme 'catppuccin :no-confirm))
 
+(use-package general
+  :ensure (:wait t)
+  :demand t
+  :config
+  (general-override-mode)
+  (general-auto-unbind-keys)
+  ;; (general-evil-setup)
+  (general-define-key
+   :keymaps 'override
+   :states '(insert normal hybrid motion visual operator emacs)
+   :prefix-map '+prefix-map
+   :prefix-command '+prefix-map
+   :prefix ","
+   :global-prefix "M-,")
+  
+  (general-create-definer dh-global-def
+    :wk-full-keys nil
+    :keymaps '+prefix-map)
+  (dh-global-def
+   "SPC" 'execute-extended-command
+   "h" (general-simulate-key "C-h" :which-key "help")
+   "!" 'shell-command
+   ":" 'eval-expression
+   "." 'repeat
+   "z" '((lambda (local) (interactive "p")
+  	 (unless repeat-mode (repeat-mode))
+  	 (let ((local current-prefix-arg)
+  	       (current-prefix-arg nil))
+  	   (call-interactively (if local #'text-scale-adjust #'global-text-scale-adjust))))
+         :which-key "Zoom")
+   "?" 'which-key-show-top-level
+   )
+  (general-create-definer dh-global-leader
+    :keymaps 'override
+    :states '(insert normal hybrid motion virual operator)
+    :prefix ", m"
+    :non-normal-prefix "M-, m"
+    "" '(:ignore t
+         :which-key
+         (lambda (arg)
+  	 (cons (cadr (split-string (car arg) " "))
+  	       (replace-regexp-in-string "-mode$" "" (symbol-name major-mode)))))
+    )
+  (defmacro +general-global-menu! (name prefix-key &rest body)
+    "Create a definer named +general-global-NAME wrapping dh-global-def.
+    Create prefix map: +general-global-NAME-map. Prefix bindings in BODY with PREFIX-KEY."
+    (declare (indent 2))
+    (let* ((n (concat "+general-global-" name))
+  	 (prefix-map (intern (concat n "-map"))))
+      `(progn
+         (general-create-definer ,(intern n)
+  	 :wrapping dh-global-def
+  	 :prefix-map (quote ,prefix-map)
+  	 :prefix ,prefix-key
+  	 :wk-full-keys nil
+  	 "" '(:ignore t :which-key ,name))
+         (,(intern n) ,@body)))
+    )
+  (+general-global-menu! "application" "a"
+    "p" '(:ignore t "elpaca")
+    "pb" 'elpaca-browse
+    "pr" '(
+  	 (lambda () (interactive)
+  	   (let ((current-prefix-arg (not current-prefix-arg))
+  		 (this-command 'elpaca-rebuild))
+  	     (call-interactively #'elpaca-rebuild)))
+  	 :which-key "rebuild")
+    "pm" 'elpaca-manager
+    "pl" 'elpaca-log
+    "pi" 'elpaca-info
+    "pI" '((lambda () (interactive) (info "Elpaca"))
+  	 :which-key "elpaca-info")
+    "ps" 'elpaca-status
+    "pt" 'elpaca-try
+    "pv" 'elpaca-visit
+    )
+  (+general-global-menu! "buffer" "b"
+    "d" 'kill-current-buffer
+    "i" 'consult-buffer
+    "o" '((lambda () (interactive) (switch-to-buffer nil))
+  	:which-key "other-buffer")
+    "p" 'previous-buffer
+    "r" 'rename-buffer
+    "R" 'revert-buffer
+    "M" '((lambda () (interactive) (switch-to-buffer "*Messages*"))
+  	:which-key "messages-buffer")
+    "n" 'next-buffer
+    "s" 'scratch-buffer
+    "TAB" '((lambda () (interactive) (switch-to-buffer nil))
+  	  :whick-key "other-buffer")
+    )
+  (+general-global-menu! "bookmark" "B")
+  (+general-global-menu! "eval" "e"
+    "b" 'eval-buffer
+    "d" 'eval-defun
+    "e" 'eval-expression
+    "p" 'pp-eval-last-sexp ; pp = pretty print
+    "s" 'eval-last-sexp)
+  (+general-global-menu! "file" "f"
+    "d" '((lambda (&optional arg) (interactive "P")
+  	  (let ((buffer (when arg (current-buffer))))
+  	    (diff-buffer-with-file buffer)))
+  	:which-key "diff-with-file")
+    "e" '(:ignore t :which-key "edit")
+    "ec" '((lambda () (interactive) (find-file "~/.emacs.d/config.org"))
+  	:which-key "Open Emacs Config")
+    "ed" '((lambda () (interactive) (find-file-existing literate-file)(widen))
+  	 :which-key "dotfile")
+    "f" 'find-file
+    "p" 'find-function-at-point
+    "P" 'find-function
+    "r" 'consult-recent-file
+    "R" 'rename-file-and-buffer
+    "s" 'save-buffer
+    "v" 'find-variable-at-point
+    "V" 'find-variable
+    )
+  (dh-global-def
+    "q" '(:ignore t :which-key "quit")
+    "qr" 'restart-emacs
+    "qs" 'save-buffers-kill-terminal
+    ))
+
 (use-package evil
   :demand t
   :ensure t
@@ -74,15 +199,31 @@
   (setq evil-want-integration t)
   (setq evil-want-keybinding nil)
   (setq evil-want-C-u-scroll t)
-  (setq evil-want-C-i-jump nil)
+  (unless (display-graphic-p)
+    (setq evil-want-C-i-jump nil))
   (setq evil-respect-visual-line-mode t)
   (setq evil-undo-system 'undo-redo)
   :config
   (evil-set-initial-state 'dashboard-mode 'emacs)
   (evil-set-initial-state 'elpaca-log-mode 'emacs)
   (evil-mode 1)
-  (evil-global-set-key 'insert "<hangul>" 'toggle-input-method)
+  (when (fboundp #'general-define-key)
+    (general-define-key
+     :states '(motion)))
+  ;; (evil-global-set-key 'insert (kbd<hangul>) 'toggle-input-method)
   )
+
+(use-package evil-surround
+  :ensure t
+  :after evil
+  :config
+  (global-evil-surround-mode 1))
+
+(use-package evil-commentary
+  :ensure t
+  :after evil
+  :config
+  (evil-commentary-mode))
 
 (use-package evil-collection
   :ensure t
@@ -91,7 +232,12 @@
   (evil-collection-init))
 
 (use-package evil-numbers
-  :ensure t)
+  :ensure t
+  :after (general)
+  :init
+  (general-define-key :states '(normal)
+   "C-a" 'evil-numbers/inc-at-pt
+   "C-s" 'evil-numbers/dec-at-pt))
 
 (use-package corfu
   :ensure t
@@ -106,6 +252,9 @@
 	       cape-capf-debug #'cape-dict)))
   :init
   (global-corfu-mode)
+  :bind (
+	 :map corfu-map
+	      (""))
   )
 
 (use-package vertico
@@ -133,6 +282,7 @@
   ;; (minibuffer-prompt-properties
    ;; '(read-only t cursor-intangible t face minibuffer-prompt))
   ;; (text-mode-ispell-word-completion nil)
+
   )
 
 (use-package cape
@@ -206,6 +356,8 @@
   :config
   (dashboard-setup-startup-hook))
 
+(desktop-save-mode 1)
+
 (set-face-attribute 'default nil
 		    :height 150
 		    :family "JetBrainsMono Nerd Font")
@@ -227,10 +379,29 @@
 (use-package org
   :ensure t
   :bind
-  (
-   :map org-mode-map
+  (:map org-mode-map
    ("C-c <up>" . org-priority-up)
    ("C-c <down>" . org-priority-down))
+  :config
+  (dh-global-leader
+    :keymaps '(org-mode-map)
+    "'" '(org-edit-special
+  	:which-key "edit code block")
+    "b" '(:ignore t :which-key "Babel")
+    "bt" 'org-babel-tangle
+    "c" 'org-capture
+    "h" 'consult-org-heading
+    "s" '(:ignore t :which-key "Src")
+    "st" 'org-insert-structure-template
+    )
+  (dh-global-leader
+    :keymaps '(org-src-mode-map lisp-mode-shared-map)
+    "'" 'org-edit-src-exit
+    "k" 'org-edit-src-abort
+    )
+  (with-eval-after-load 'org
+    (add-to-list 'org-structure-template-alist
+  	       '("se" . "src emacs-lisp\n")))
   )
 
 (use-package org-super-agenda :ensure t)
